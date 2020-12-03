0eb711bc61930a2e97167bf2c75b4c9e02260a11
shweta2902 <74788666+shweta2902@users.noreply.github.com>
2020-12-03 23:19:36 +0530

    Update dustbin.js

--- dustbin.js
+++ dustbin.js
@@ -8,7 +8,7 @@ class Dustbin
 		this.dustbinHeight=213;
 		this.wallThickness=20;
 		
-		this.image=loadImage("sprites/dustbin.png")
+		this.image=loadImage("dustbin.png")
 		this.bottomBody=Bodies.rectangle(this.x, this.y, this.dustbinWidth, this.wallThickness, {isStatic:true})
 		this.leftWallBody=Bodies.rectangle(this.x-this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
 		
